850c5b12ffaa133334b6b7b9b556856e747e5bc1
Milyddc <emilyfer1703@gmail.com>
2026-07-27 14:02:28 -0300

    Update registro.php

---
+++ BACKEND/API USUARIOS/registro.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once "../../config/headers.php";
+require_once "../../controllers/UsuarioController.php";
+
+$controller = new UsuarioController();
+
+$datos = json_decode(file_get_contents("php://input"), true);
+
+echo json_encode($controller->registrar($datos));
