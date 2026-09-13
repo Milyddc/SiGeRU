7c862ea71d8d34df9691c7780b81a43349b5c0c3
Milyddc <emilyfer1703@gmail.com>
2026-09-13 12:47:06 -0300

    Delete BACKEND ver1 directory

--- BACKEND/API USUARIOS/listarusuarios.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once "../../config/headers.php";
-require_once "../../controllers/UsuarioController.php";
-
-$controller = new UsuarioController();
-
-echo json_encode($controller->listarUsuarios());
